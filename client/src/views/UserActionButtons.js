1fb022d5a69e8ceba60afd06eed9675eb4c28609
TedtheDev <tylerjedmunds@gmail.com>
2018-02-25 19:54:09 -0600

    changed gitignore; fixed user action buttons to show add on right side

--- client/src/views/UserActionButtons.js
+++ client/src/views/UserActionButtons.js
@@ -9,15 +9,18 @@ class UserActionButtons extends Component {
     
     promptUserToAddListItem() {
         const result = window.prompt('Enter the list item you want to add');
+        console.log(this.props)
         this.props.addListItem(result);
     }
 
     render() {
         return (
             <div className="user-buttons">
-                <Button secondary onClick={this.props.openAllListItems} type="button">Open All</Button>
-                <Button secondary onClick={this.props.closeAllListItems} type="button">Close All</Button>
-                <Button secondary onClick={this.props.toggleAllListItems} type="button">Toggle All</Button>
+                <div>
+                    <Button secondary onClick={this.props.openAllListItems} type="button">Open All</Button>
+                    <Button secondary onClick={this.props.closeAllListItems} type="button">Close All</Button>
+                    <Button secondary onClick={this.props.toggleAllListItems} type="button">Toggle All</Button>
+                </div>
                 <Button primary className="btn-add" onClick={this.promptUserToAddListItem} type="button">Add</Button>
             </div>
         )
